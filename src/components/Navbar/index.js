b7ea68ea7f9d5b573a1ab7c317896765c49a8112
App dev <appdev@Apps-MacBook-Air.local>
2021-01-02 15:54:40 +0530

    navbar_2_react

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Logo</h1>
+                </NavLink>
+                <Bars/>
+                <NavMenu>
+                    <NavLink to="/about" activeStyle>About</NavLink>
+                    <NavLink to="/services" activeStyle>Services</NavLink>
+                    <NavLink to="/contact-us" activeStyle>Contact Us</NavLink>
+                    <NavLink to="/sign-up" activeStyle>Sign Up</NavLink>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavMenu>
+                {/* <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn> */}
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar;
